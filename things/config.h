ccc6a84bbd624c651350a03c38dcc5f319005a9e
Trammell hudson <hudson@trmm.net>
2018-02-24 11:36:10 -0500

    automation.local

--- things/config.h
+++ things/config.h
@@ -3,10 +3,16 @@
 /*
  * WiFi configuration for the things at the NYC Resistor space
  */
+#if 1
 #define WIFI_SSID	"NYCR24"
 #define WIFI_PASSWORD	"clubmate"
+#define MQTT_SERVER	"automation.local"
+#else
+#define WIFI_SSID	""
+#define WIFI_PASSWORD	""
+#define MQTT_SERVER	"10.0.1.43"
+#endif
 
-#define MQTT_SERVER	"192.168.1.7"
 #define MQTT_PORT	1883
 #define MQTT_USER	""
 #define MQTT_PASSWORD	""
